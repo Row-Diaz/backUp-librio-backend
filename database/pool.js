9c4d87d21eb459d6ae8790afb4d68ee2627dd6e1
Row <axlrower@gmail.com>
2025-11-10 15:38:17 -0300

    cambio de clave

--- database/pool.js
+++ database/pool.js
@@ -6,7 +6,7 @@ dotenv.config();
 const pool = new Pool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || '2504',
+  password: process.env.DB_PASSWORD || '4312',
   database: process.env.DB_NAME || 'libreria',
   port: process.env.DB_PORT || 5432,
   allowExitOnIdle: true,
